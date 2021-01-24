fcfe76d7bca825ab4a4964855f7ff82cb832a462
AlexeyVasiluk <vasiluk.o@gmail.com>
2021-01-24 16:04:36 +0200

    test logic created

---
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -0,0 +1,41 @@
+
+import React from 'react';
+import classes from './FinishedQuiz.css';
+
+const FinishedQuiz = (props) => {
+    const successCount = Object.keys(props.results).reduce((total, key) => {
+        if (props.results[key] === 'success') {
+            total++;
+        }
+        return total;
+    }, 0);
+
+    return (
+        <div className={classes.FinishedQuiz}>
+            <ul>
+                {
+                    props.quiz.map((quizItem, index) => {
+                        const cls = [
+                            'fa',
+                            props.results[quizItem.id] === 'error' ? 'fa-times' : 'fa-check',
+                            classes[props.results[quizItem.id]]
+                        ];
+                        return (
+                            <li key={index}>
+                                <strong>{index + 1}.</strong>&nbsp;
+                                {quizItem.question}
+                                <i className={cls.join(' ')}/>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+            <p>Правильно {successCount} из {props.quiz.length}</p>
+            <div>
+                <button onClick={props.onRetry}>Повторить</button>
+            </div>
+        </div>
+    )
+};
+
+export default FinishedQuiz;
